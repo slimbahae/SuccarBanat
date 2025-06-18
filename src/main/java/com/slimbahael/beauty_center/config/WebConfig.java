c31e482b7b6dc079d1ba5f24c6dc108a6021c18c
Bahae <bahaesliman@gmail.com>
2025-06-18 18:56:49 +0000

    Production ready configuration

--- src/main/java/com/slimbahael/beauty_center/config/WebConfig.java
+++ src/main/java/com/slimbahael/beauty_center/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:3000", "http://127.0.0.1:3000")
+                .allowedOriginPatterns("http://localhost:3000", "http://127.0.0.1:3000", "https://succarbanat.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
